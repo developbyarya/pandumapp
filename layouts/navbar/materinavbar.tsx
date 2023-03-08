2a1aecb9102f4245fcdd0933a22ec77a9716d173
Arya Wicaksana <aryain3rd@gmail.com>
2023-03-08 09:07:31 +0700

    mvp01

---
+++ layouts/navbar/materinavbar.tsx
@@ -0,0 +1,25 @@
+import BackButton from "@/components/button/backbutton";
+
+export default function MateriNavbar({
+  value,
+  total,
+}: {
+  value: number;
+  total: number;
+}) {
+  const width = value <= total ? (value / total) * 100 : 100;
+  return (
+    <nav className="flex flex-row justify-between w-full">
+      <BackButton to="/app/materi/aksara-dasar" />
+      <div className="bg-slate-300 w-2/3 rounded-full h-6">
+        <div
+          className={`bg-blue-700 h-full rounded-full`}
+          style={{ width: width + "%" }}
+        ></div>
+      </div>
+      <p>
+        {value}/{total}
+      </p>
+    </nav>
+  );
+}
